b48c8e7eca9c16495673e01aaed2341b8487da44
Ankit Bose <abinfo2310@gmail.com>
2024-10-05 03:15:15 +0530

    Initial Configurations Done and all constants added.

---
+++ lib/Utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Utilities {
+  static const Color DARK_BLUE = Color(0xFF0C54BE);
+  static const Color NAVY_BLUE = Color(0xFF303F60);
+  static const Color GRAY_SHADE_1 = Color(0xFFF5F9FD);
+  static const Color GRAY_SHADE_2 = Color(0xFFCED3DC);
+  
+  
+
+}
